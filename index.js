2d8306d4c936bfdf8518aea91b21ffde5a7079ae
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-04-03 12:30:54 +0300

    adds event listener

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+  let button = document.getElementById("button");
+  const div = document.getElementsByTagName("div")[1];
+  button.addEventListener("click", () => document.body.remove());
 }
+addingEventListener();
